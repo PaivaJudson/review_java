b8a16aa9b827412942a875552d7bc98ca1c5bfa3
Judson Paiva <judson.paiva@hotmail.com>
2025-11-24 21:26:45 +0100

    resolucao do 5 exercicio e documentacao package-info.java

--- exercicio_5/Categoria.java
+++ exercicio_5/Categoria.java
@@ -1,8 +1,10 @@
 package exercicio_5;
 
 /**
- * <strong>Modelagem entre um livro e a categoria a que pertence (por exemplo,
+ * <strong>Modelagem entre um livro e a categoria a que pertence</strong> (por exemplo,
  * “Tecnologia”, “Romance”, ...), utilizando “tem um” para representar a classificação.
+ * 
+ * <p>Esta classe armazena o nome e uma breve descrição da categoria.
  * 
  * @author Judson Paiva
  * @version 1.0
@@ -11,10 +13,23 @@ package exercicio_5;
 
 public class Categoria {
 	
+	/**
+     * O nome da categoria (e.g., "Romance", "História").
+     */
 	private String nome;
+	
+	/**
+     * Uma descrição mais detalhada da categoria.
+     */
 	private String descricao;
 	
-	
+	/**
+     * Construtor da classe Categoria.
+     * Cria uma nova categoria com um nome e uma descrição definidos.
+     *
+     * @param nome O nome da categoria.
+     * @param descricao Uma breve descrição do tipo de conteúdo que a categoria abrange.
+     */
 	public Categoria(String nome, String descricao) {
 		this.nome = nome;
 		this.descricao = descricao;
---
+++ exercicio_5/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Contém as classes modelo para o exercício 5, incluindo {@link Livro} e
+ * {@link Categoria}.
+ *
+ * <p>O principal objetivo deste pacote é demonstrar a relação de composição
+ * entre classes e o uso correto do Javadoc.
+ *
+ * @since 2025-11-24
+ * @author Judson Paiva
+ */
+package exercicio_5;
